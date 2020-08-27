336c4903fe4fa50495b8a2236c5aa127446c7ccc
acanalpay <ardaakcabuyuk@gmail.com>
2020-08-27 20:03:25 +0300

    editing is an option now

--- my_blog/blogs/urls.py
+++ my_blog/blogs/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('addblog/', views.add_blog),
     re_path(r'^deleteblog/(?P<pk>\d+)/$', views.delete_blog, name='delete_blog'),
     path('', ListView.as_view(queryset=Blog.objects.all().order_by('-publish_date'), template_name = 'blogs/posts.html')),
-    re_path(r'^blogs/(?P<pk>\d+)$', DetailView.as_view(model=Blog, template_name='blogs/post.html'))
+    re_path(r'^blogs/(?P<pk>\d+)$', DetailView.as_view(model=Blog, template_name='blogs/post.html')),
+    re_path(r'^editblog/(?P<pk>\d+)/$', views.edit_blog, name='edit_blog'),
 ]
--- my_blog/blogs/views.py
+++ my_blog/blogs/views.py
@@ -20,6 +20,24 @@ def add_blog(request):
             return render(request,'blogs/add_blog.html')
 
 def delete_blog(request, pk=None):
-    post = Blog.objects.get(id = pk)
-    post.delete()
+    blog = Blog.objects.get(id=pk)
+    blog.delete()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def edit_blog(request, pk=None):
+    blog = Blog.objects.get(id=pk)
+    dic = ({"title":blog.title, "abstract":blog.abstract, "body":blog.body})
+    dict2 = {"dict1":dic}
+
+    if request.method == 'POST':
+        if request.POST.get('title') or request.POST.get('abstract') or request.POST.get('body'):
+            blog.title= request.POST.get('title')
+            blog.abstract= request.POST.get('abstract')
+            blog.body= request.POST.get('body')
+            blog.save()
+            dic = ({"title":blog.title, "abstract":blog.abstract, "body":blog.body})
+            dict2 = {"dict1":dic}
+            return render(request, 'blogs/edit_blog.html', dict2)
+
+    else:
+        return render(request, 'blogs/edit_blog.html', dict2)
