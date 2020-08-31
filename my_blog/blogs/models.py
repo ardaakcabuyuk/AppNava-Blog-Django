a99ed78c3e099c1cdd0b64db05d68fc2ddd4c70e
acanalpay <ardaakcabuyuk@gmail.com>
2020-08-31 13:03:26 +0300

    solved raw html output in body

--- my_blog/blogs/models.py
+++ my_blog/blogs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
+    slug = models.SlugField()
     abstract = models.TextField()
     body = models.TextField()
     publish_date = models.DateTimeField(auto_now_add=True)
